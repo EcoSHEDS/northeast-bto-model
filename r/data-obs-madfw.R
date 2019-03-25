2545ad8f7e0712d4b656c5389f175be2fd70fd8e
Jeff Walker <walker.jeff.d@gmail.com>
2019-03-25 10:45:27 -0400

    rename and update data-obs-madfw

--- r/data-obs-madfw.R
+++ r/data-obs-madfw.R
@@ -3,7 +3,7 @@
 # -> ../data/obs/madfw-ebt.csv
 
 start <- lubridate::now(tzone = "US/Eastern")
-cat("starting data-obs-madfw-ebt: ", as.character(start, tz = "US/Eastern"), "\n", sep = "")
+cat("starting data-obs-madfw: ", as.character(start, tz = "US/Eastern"), "\n", sep = "")
 
 suppressPackageStartupMessages(library(RPostgreSQL))
 suppressPackageStartupMessages(library(tidyverse))
@@ -24,6 +24,7 @@ db <- src_postgres(
 
 # load raw data (presence only)
 # source: 20181206 - MA Wildlife Data/MassWildlife coldwater spp/Brook_Trout_nat_prod.shp
+cat("loading raw data from ../data/obs/madfw-ebt-raw.csv...")
 df <- read_csv("../data/obs/madfw-ebt-raw.csv", col_types = cols(
   .default = col_character(),
   latitude = col_double(),
@@ -31,6 +32,7 @@ df <- read_csv("../data/obs/madfw-ebt-raw.csv", col_types = cols(
   year = col_double()
 )) %>%
   select(latitude, longitude, year)
+cat("done\n")
 
 find_featureid <- function (latitude, longitude) {
   x <- dbGetQuery(
@@ -44,10 +46,22 @@ find_featureid <- function (latitude, longitude) {
 # find_featureid(df$latitude[1], df$longitude[1])
 
 # find featureid for each row
+cat("assigning featureids from lat/lon...")
 df <- df %>%
   mutate(
     featureid = map2_int(latitude, longitude, ~ find_featureid(.x, .y))
   )
+cat("done\n")
 
-# export
+# export ------------------------------------------------------------------
+
+cat("saving cleaned madfw dataset to ../data/obs/madfw-ebt.csv...")
 write_csv(df, "../data/obs/madfw-ebt.csv")
+cat("done\n")
+
+# end ---------------------------------------------------------------------
+
+end <- lubridate::now(tzone = "US/Eastern")
+elapsed <- as.numeric(difftime(end, start, tz = "US/Eastern", units = "sec"))
+
+cat("finished data-obs-madfw: ", as.character(end, tz = "US/Eastern"), " (elapsed = ", round(elapsed, digits = 1), " sec)\n", sep = "")
